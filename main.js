448178344a7b3ca8ad7e571248a24ce64786de45
Leandro Vieira <leandrown@outlook.com>
2020-01-09 17:45:22 -0300

    Operadores rest/spread - parte 2

--- main.js
+++ main.js
@@ -19,3 +19,32 @@ const [a, b, ...c] = arr;
 console.log(a);
 console.log(b);
 console.log(c);
+
+// Tambem pode ser aplicado em funcoes
+function soma(a, b, ...params) {
+    return params;
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6, 7));
+
+
+// SPREAD - operador que repassa as informacoes de
+// um objeto ou array para outra estrutura de dados
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+// Recurso muito utilizado no React
+const usuario1 = {
+    nome: 'Leandro',
+    idade: 36,
+    empresa: 'Mandrillus'
+};
+
+const usuario2 = { ...usuario1, nome: 'Carlos Alberto' };
+
+console.log(usuario2);

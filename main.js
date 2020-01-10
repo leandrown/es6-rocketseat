6cad60c4e008840e3b5c278731f04c4266281bf9
Leandro Vieira <leandrown@outlook.com>
2020-01-10 11:17:09 -0300

    Template literals

--- main.js
+++ main.js
@@ -1,50 +1,8 @@
-// REST - operador que pega o "resto do objeto"
+const nome = "Leandro";
+const idade = 36;
 
-const usuario = {
-    nome: 'Leandro',
-    idade: 36,
-    empresa: 'Mandrillus'
-};
+// Concatenacao tradicional
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
 
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// Podemos usar também em um vetor/array
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// Tambem pode ser aplicado em funcoes
-function soma(a, b, ...params) {
-    return params;
-}
-
-console.log(soma(1, 2, 3, 4, 5, 6, 7));
-
-
-// SPREAD - operador que repassa as informacoes de
-// um objeto ou array para outra estrutura de dados
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-// Recurso muito utilizado no React
-const usuario1 = {
-    nome: 'Leandro',
-    idade: 36,
-    empresa: 'Mandrillus'
-};
-
-const usuario2 = { ...usuario1, nome: 'Carlos Alberto' };
-
-console.log(usuario2);
+// Usando Template Literals
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
